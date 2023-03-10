81a9dff1181680a0f0db8be2cf297235e33bf3f6
Kokonod Ray <kokonodray2001@gmail.com>
2023-03-10 23:20:06 +0530

    first commit

---
+++ routes/socketIoCode.js
@@ -0,0 +1,48 @@
+const {instrument} =  require('@socket.io/admin-ui');
+var express =  require('express');
+const { Socket } = require('socket.io');
+var router =  express.Router();
+var io =  require('socket.io')(3000,{
+    cors :{
+        origin :['https://admin.socket.io' , 'http://localhost:5000'],
+        methods: ["GET", "POST"],
+        allowedHeaders: ["Access-Control-Allow-Credentials"],
+        credentials: true
+    }   
+    
+});
+//connection to socket
+io.on('connection',(socket)=>{ //  socket is the client and io is the server
+    
+    console.log("connected");
+    //listening to the client event
+    socket.on('send-message',(message,room)=>{ //  recive from the client 
+        //io.emit('recived-message',"broadcasted") // io.emit send the message to all the socket connect to the server
+        if(room == ''){
+            socket.broadcast.emit('recived-message',message)
+        }
+        else
+            socket.to(room).emit('recived-message',message);
+        //socket.broadcast.emit('recived-message',"recived from socket id " + socket.id);//to broadcast message to all the connected socket from the given socket
+        
+    })
+    socket.on('join-room',(room)=>{
+        socket.join(room);
+    })  
+   
+
+})
+
+instrument(io, {
+    auth: false,
+    mode: "development",
+  });
+
+router.get('/',(req,res)=>{
+    res.render('editorView');
+   
+});
+//instrument(io,{auth :false});
+
+module.exports = router;
+
